ef1bc881ea78894151c1f461f263de529ecc3cc9
Luis Henrique Trindade <luishenriquetp@gmail.com>
2025-11-25 10:26:28 -0300

    Add files via upload

---
+++ brightup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# Diminui o brilho em 1%
+brightnessctl set 1%+
+
+# Obtém o brilho atual formatado em percentual (ex: 79%)
+BRILHO=$(brightnessctl | grep 'Current brightness' | awk '{print $4}' | tr -d '()%')
+
+# Exibe uma notificação com o brilho atual
+notify-send -t 1 -a "Configurações do Monitor" "Brilho da Tela" "${BRILHO}%"
